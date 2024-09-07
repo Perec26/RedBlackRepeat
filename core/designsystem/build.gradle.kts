1fbe8489df8a387ecdb2e35f286c1f8537eb45aa
valera <zadvor93@gmail.com>
2024-09-07 18:52:15 +0300

    0.1.1.8 update dependencies

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 }
 
