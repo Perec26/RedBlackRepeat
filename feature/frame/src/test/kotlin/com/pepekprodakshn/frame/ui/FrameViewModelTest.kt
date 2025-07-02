3c59b9b2e4b743fa13a2ab3ba3ee3bac33920f0b
valera <zadvor93@gmail.com>
2025-07-02 17:57:16 +0300

    check ci/cd testing

--- feature/frame/src/test/kotlin/com/pepekprodakshn/frame/ui/FrameViewModelTest.kt
+++ feature/frame/src/test/kotlin/com/pepekprodakshn/frame/ui/FrameViewModelTest.kt
@@ -113,7 +113,7 @@ internal class FrameViewModelTest : FreeSpec(
 
                         "Then: foul points should be set" {
                             with(viewModel.viewState) {
-                                foulUI.points shouldBe 1
+                                foulUI.points shouldBe 2
                             }
                         }
                     }
