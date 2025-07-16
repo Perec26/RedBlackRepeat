4fc1937a1903a043c1a00fb3cff86a0e16034b38
valera <zadvor93@gmail.com>
2025-07-16 22:46:31 +0300

    0.3 fix test and ci

--- data/table/src/main/java/com/pepekprodakshn/table/model/TableState.kt
+++ data/table/src/main/java/com/pepekprodakshn/table/model/TableState.kt
@@ -4,7 +4,7 @@ data class TableState(
     val firstPlayerPoints: Int = 0,
     val secondPlayerPoints: Int = 0,
     val redsCount: Int = 15,
-    val lowestPriceBall: Ball = Ball.RED,
+    val lowestValueBall: Ball = Ball.RED,
     val nextIsColor: Boolean = false,
     val frameBreak: FrameBreak? = null,
     val isFirstPlayerSelected: Boolean = true,
